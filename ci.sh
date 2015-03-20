49aa027711f292e9f298b9a58db914ce9ff28f65
Matthew O'Gorman <mog@rldn.net>
2015-03-20 17:06:45 -0500

    hey its almost really real

---
+++ ci.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#this is where continuous integration script will live
+Project="cleaver"
+
+copy_files() {
+    local new_copy="$1"
+    local old_copy="$2"
+    local versioned=`git rev-parse --short HEAD`"_$1"
+    if diff "$new_copy" "$old_copy" 2>/dev/null ; then
+	echo Same file nothing to do
+    else
+	echo File changed make copies.
+	cp "$1" "$2"
+	cp "$1" "/home/gitlab_ci_runner/artifacts/${Project}/build/"`git rev-parse --short HEAD`_"$1"
+    fi    
+}
+
+git submodule update --init
+ls -la
+cd hardware
+make clean
+make CUSTOMPCB="/usr/src/pcb/src/pcb" \
+     CUSTOMPCB_ARGS="--photo-mask-colour red \
+     --photo-silk-colour white --photo-plating  tinned"
+make CUSTOMPCB="/usr/src/pcb/src/pcb" \
+     CUSTOMPCB_ARGS="--photo-mask-colour red \
+     --photo-silk-colour white  --photo-plating  tinned" photos
+ls black_board.png board.png bom order xy schematic.png schematic.pdf gerbers/${Project}_make.bottom.gbr > /dev/null 2>&1
+if [ "$?" != "0" ]; then
+    echo "I failed to create all the files I should have. build failed"
+    exit 1
+fi
+New_project=`git diff HEAD^ HEAD -- ${Project}.pcb|wc -l`
+New_schematic=`git diff HEAD^ HEAD -- ${Project}.sch|wc -l`
+
+if [ "$New_project" == "0" ]; then
+    echo "No changes to ${Project} don't bother saving image."
+else
+    copy_files board.png ~/artifacts/${Project}/board.png
+    File=`ls *.zip|grep -v oshpark`
+    cp "${File}" gerbers.zip
+    copy_files gerbers.zip ~/artifacts/${Project}/gerbers.zip
+fi
+
+if [ "$New_schematic" == "0" ]; then
+    echo "No changes to schematic don't bother saving the image."
+else
+    copy_files schematic.png ~/artifacts/${Project}/schematic.png
+fi
---
+++ hardware/cleaver.sch
@@ -0,0 +1,27 @@
+v 20130925 2
+C 40600 38100 1 0 0 include-1.sym
+{
+T 40700 38200 5 10 1 1 0 0 1
+device=include
+T 40600 38400 5 10 1 1 0 0 1
+refdes=ICO1
+T 41300 38200 5 10 1 1 0 0 1
+footprint=oshw
+}
+C 40300 37800 0 0 0 title-A2.sym
+T 56600 38500 14 10 1 0 0 0 1
+Spam
+T 56500 38200 14 8 1 0 0 0 1
+http://dev.meatstand.com/meatstand/spam.git
+T 57600 37900 14 10 1 0 0 0 1
+1
+T 58100 37900 14 10 1 0 0 0 1
+1
+T 60500 37900 14 10 1 0 0 0 1
+Matthew O'Gorman <mog@rldn.net>
+T 60400 38200 14 10 1 0 0 0 1
+@version@
+T 62000 38200 14 10 1 0 0 0 1
+@git@
+T 60800 38500 14 10 1 0 0 0 1
+This schematic is licensed GPLv3
