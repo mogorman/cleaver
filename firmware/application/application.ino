c3bb01b13efacfc30fc34b09a254a6729e7bc0b9
Matthew O'Gorman <mog@rldn.net>
2015-04-05 21:46:43 -0500

    beginning to write attiny arduino based firmware for soldering iron

---
+++ firmware/application/application.ino
@@ -0,0 +1,15 @@
+#define LED 13
+// the setup routine runs once when you press reset:
+void setup()
+{                
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(5000);               // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(5000);               // wait for a secondIGH is the voltage level)
+  Serial.println("hello there");
+}
